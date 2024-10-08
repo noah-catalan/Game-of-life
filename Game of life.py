8eb3c71372231ca752aed5ea3762d9aa263e290b
Noah Catalán Rosell <ncatalanrosell@cifpfbmoll.eu>
2024-10-08 17:06:00 +0200

    Add files via upload

---
+++ Game of life.py
@@ -0,0 +1,103 @@
+# EL JUEGO DE LA VIDA
+
+import pygame 
+import numpy as np
+import time
+
+pygame.init()
+
+width, height = 700, 700
+screen = pygame.display.set_mode((height, width))
+
+bg = 25, 25, 25
+
+screen.fill(bg)
+
+nxC, nyC = 50, 50
+
+dimCW = width / nxC
+dimCH = height / nyC
+
+
+gameState = np.zeros((nxC, nyC))
+
+
+
+
+gameState[21,21] = 1
+gameState[22,22] = 1
+gameState[22,23] = 1
+gameState[21,23] = 1
+gameState[20,23] = 1
+
+pauseEx = False
+
+# Bucle de ejecución
+running = True
+while running:
+    newGameState = np.copy(gameState)
+
+    screen.fill(bg)
+
+    time.sleep(0.1)
+
+        # Control de eventos
+    
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            pauseEx = not pauseEx
+
+    
+
+    for y in range(0, nyC):
+        for x in range(0, nxC):
+
+            if not pauseEx:
+
+                n_neigh = gameState[(x - 1) % nxC, (y - 1) % nyC] + \
+                        gameState[(x    ) % nxC, (y - 1) % nyC] + \
+                        gameState[(x + 1) % nxC, (y - 1) % nyC] + \
+                        gameState[(x - 1) % nxC, (y    ) % nyC] + \
+                        gameState[(x + 1) % nxC, (y    ) % nyC] + \
+                        gameState[(x - 1) % nxC, (y + 1) % nyC] + \
+                        gameState[(x    ) % nxC, (y + 1) % nyC] + \
+                        gameState[(x + 1) % nxC, (y + 1) % nyC] 
+                
+                if gameState[x, y] == 0 and n_neigh == 3:
+                    newGameState[x, y] = 1
+
+                elif gameState[x, y] == 1 and (n_neigh < 2 or n_neigh > 3):
+                    newGameState[x, y] = 0
+                    
+            poly = [((x)   * dimCW,  y    * dimCH), 
+                    ((x+1) * dimCW,  y    * dimCH),
+                    ((x+1) * dimCW, (y+1) * dimCH),
+                    ((x)   * dimCW, (y+1) * dimCH)]
+            
+            if newGameState[x, y] == 0:
+                pygame.draw.polygon(screen, (128, 128, 128), poly, 1)
+            
+            
+            else:
+                pygame.draw.polygon(screen, (255, 255, 255), poly, 0)
+    
+
+
+    mouseClick = pygame.mouse.get_pressed()
+
+    if sum(mouseClick) > 0:
+        posX, posY = pygame.mouse.get_pos()
+        celX, celY = int(np.floor(posX / dimCW)), int(np.floor(posY / dimCH))
+        newGameState[celX, celY] = not mouseClick[2]
+
+    gameState = np.copy(newGameState)
+            
+    
+    pygame.display.flip()
+
+
+
+ 
